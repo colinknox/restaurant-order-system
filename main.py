4e7c128cc750063a6298ce872dd1f321a72035e3
ColinKnox <colinwknox@gmail.com>
2026-02-27 11:44:43 -0500

    created DineInOrder and TakeoutOrder classes

--- main.py
+++ main.py
@@ -15,25 +15,46 @@ class Order:
     def get_final_total(self):
         return self.total + self.calculate_tax() + self.calculate_fees()
 
+class DineInOrder(Order):
+    def __init__(self, items, total):
+        super().__init__(items, total)
+    
+    def calculate_fees(self):
+        return 0
+    
+    def process_order(self):
+        return "Order sent to kitchen, table service"
 
+class TakeoutOrder(Order):
+    def __init__(self, items, total):
+        super().__init__(items, total)
+
+    def calculate_fees(self):
+        return 2
+    
+    def process_order(self):
+        return "Order prepared for pickup"
 
 
 
 order = Order(["Gang-Jeong", "Cheesling"], 10)
-
-print(f"""
-ITEMS: {order.items}
-TOTAL: {order.total}
-TAX: {order.calculate_tax()}
-FINAL TOTAL: {order.get_final_total()}
-""")
+dine_in_order = DineInOrder(["Burger", "Chicken fries", "Soda"], 50)
+takeout_order = TakeoutOrder(["Water"], 5)
 
 # print(f"""
 # ITEMS: {order.items}
 # TOTAL: {order.total}
 # TAX: {order.calculate_tax()}
-# CALCULATE FEES: {order.calculate_fees()}
-# PROCESS ORDER: {order.process_order()}
+# FINAL TOTAL: {order.get_final_total()}
 # """)
+
+print(f"""
+ITEMS           : {takeout_order.items}
+TOTAL           : {takeout_order.total}
+TAX             : {takeout_order.calculate_tax()}
+CALCULATE FEES  : {takeout_order.calculate_fees()}
+PROCESS ORDER   : {takeout_order.process_order()}
+FINAL TOTAL     : {takeout_order.get_final_total()}
+""")
 
 
